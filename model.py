967b1f64a2596382f9e4524b0abb5735c314f4d3
Ali Abdullah <aliabdullah.mj@outlook.com>
2026-03-18 09:37:51 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,26 +1,20 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import accuracy_score
+
 
 def train_model():
-    df = pd.read_csv('emails.csv')
 
-    X = df['text']
-    y = df['label']
+    data = pd.read_csv("phishing_small.csv")
+    
 
-    vectorizer = CountVectorizer()
-    X_vectorized = vectorizer.fit_transform(X)
+    X = data["text_combined"].astype(str)
+    y = data["label"]
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X_vectorized, y, test_size=0.2
-    )
+    vectorizer = TfidfVectorizer()
+    X_vector = vectorizer.fit_transform(X)
 
     model = MultinomialNB()
-    model.fit(X_train, y_train)
+    model.fit(X_vector, y)
 
-    predictions = model.predict(X_test)
-    accuracy = accuracy_score(y_test, predictions)
-
-    return model, vectorizer, accuracy
+    return model, vectorizer
