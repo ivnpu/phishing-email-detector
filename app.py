665621c18ed71a2df006a2329b07ce54d9072471
Ali Abdullah <aliabdullah.mj@outlook.com>
2026-03-18 09:37:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,53 @@
-from flask import Flask, render_template, request
 from model import train_model
+import re
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-model, vectorizer, accuracy = train_model()
+model, vectorizer = train_model()
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/", methods=["GET","POST"])
 def home():
-    prediction = None
 
-    if request.method == 'POST':
-        email = request.form['email']
+    prediction = ""
+    warning = ""
+    probability = ""
+
+    if request.method == "POST":
+
+        email = request.form["email"]
+
+        links = re.findall(r'https?://\S+', email)
+
+        if links:
+            warning = "⚠ Suspicious link detected"
+
         email_vector = vectorizer.transform([email])
-        result = model.predict(email_vector)[0]
 
-        prediction = "Phishing ⚠️" if result == 1 else "Safe ✅"
+        result = model.predict(email_vector)
+       
+        prob = model.predict_proba(email_vector)
+
+        phishing_prob = round(prob[0][1] * 100,2)
+
+        probability = f"Probability: {phishing_prob}%"
+
+        if result[0] == 1 or links:
+           prediction = "Phishing Email"
+        else:
+           prediction = "Safe Email"
 
     return render_template(
-        'index.html',
+        "index.html",
         prediction=prediction,
-        accuracy=round(accuracy * 100, 2)
+        warning=warning,
+        probability=probability
     )
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
